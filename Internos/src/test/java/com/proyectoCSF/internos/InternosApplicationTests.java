ee8607a2bddff7629b864cb473e3624b00a9d373
BraianAmador <braian.amador@gmail.com>
2025-03-10 23:45:51 -0300

    Armado de springboot

---
+++ Internos/src/test/java/com/proyectoCSF/internos/InternosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoCSF.internos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
